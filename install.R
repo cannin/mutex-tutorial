5d15836b62b72c2926345c8a3d9259a77b02c1a4
cannin <cannin@gmail.com>
2019-05-05 10:58:10 -0400

    Change to just devtools

--- install.R
+++ install.R
@@ -1,5 +1,18 @@
 install.packages("devtools")
 install.packages("stringr")
 
-source("https://gist.githubusercontent.com/cannin/6b8c68e7db19c4902459/raw/installPackages.R")
-installPackages("r-requirements.txt")
+library(devtools)
+
+#install_github("BioPAX/paxtoolsr")
+#install_bitbucket("mil2041/netboxr")
+
+install_cran("rmarkdown")
+install_cran("rJava")
+install_bioc("clusterProfiler")
+install_bioc("GSEABase")
+install_bioc("enrichplot")
+install_bitbucket("mil2041/netboxr")
+install_github("BioPAX/paxtoolsr")
+
+#source("https://gist.githubusercontent.com/cannin/6b8c68e7db19c4902459/raw/installPackages.R")
+#installPackages("r-requirements.txt")
